8cc5434213e07efa36be74c22226f75b17d6104c
DannyCoop <cooperdaniel5141@gmail.com>
2020-07-06 15:36:03 -0400

    have worked on the login and made the home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    def homepage
+
+    end
+
+    def sign_in
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :users
   resources :movies
   resources :attendences, only: [:index, :new, :create]
+  get "/homepage", to: "application#homepage", as: "homepage"
+  get "/login", to: "sessions#new"
+  post "/login", to:"sessions#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
